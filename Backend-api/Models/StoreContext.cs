56e41bb135447c386f1cf0cdfb9e3bd8fbaa2c11
Diego Nicolas Gonzalez Valenzuela <diegonzalezv93@gmail.com>
2024-01-24 18:36:45 -0300

    creando context

---
+++ Backend-api/Models/StoreContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Backend_api.Models
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions<StoreContext> options):base(options)
+        {
+            
+        }
+        public DbSet<Beer> Beers { get; set; }
+        public DbSet<Brand> Brands { get; set; }
+    }
+}
