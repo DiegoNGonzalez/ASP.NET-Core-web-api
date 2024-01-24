9969b915933293ae9e237831706385bf1c2f89e0
Diego Nicolas Gonzalez Valenzuela <diegonzalezv93@gmail.com>
2024-01-24 18:33:21 -0300

    creando modelos

---
+++ Backend-api/Models/Beer.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Backend_api.Models
+{
+    public class Beer
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int BeerId { get; set; }
+        public string? Name { get; set; }
+
+        public int BrandId { get; set; }
+        [ForeignKey("BrandId")]
+        public virtual Brand Brand { get; set; }
+
+    }
+}
